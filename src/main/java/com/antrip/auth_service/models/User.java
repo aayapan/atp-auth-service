aec9382e7bf42c68e7028d8ec072f60b5addb34d
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-14 15:55:34 +0800

    (chore) create User entity

---
+++ src/main/java/com/antrip/auth_service/models/User.java
@@ -0,0 +1,31 @@
+package com.antrip.auth_service.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String displayName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false, name = "bcrypt_password")
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime dateCreated;
+
+    @Column(nullable = false)
+    private LocalDateTime dateUpdated;
+}
