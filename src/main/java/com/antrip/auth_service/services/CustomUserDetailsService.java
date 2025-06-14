ec9ce8cb12f7162c29f952830fbcef9370824086
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-14 20:02:48 +0800

    (feat) implement CustomUserDetailsService that will validate user credentials entered during login

---
+++ src/main/java/com/antrip/auth_service/services/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.antrip.auth_service.services;
+
+import com.antrip.auth_service.models.User;
+import com.antrip.auth_service.models.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
