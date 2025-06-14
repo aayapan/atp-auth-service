00a192b9fe786901a136131456d0f5b4477e9e9e
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-14 20:00:36 +0800

    (chore) custom exception for User Already Exists

---
+++ src/main/java/com/antrip/auth_service/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.antrip.auth_service.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
+
