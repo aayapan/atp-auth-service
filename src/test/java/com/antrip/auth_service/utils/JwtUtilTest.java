a77bc342b118a654f6fa2fcfa6e7e3f2badfd6a3
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-29 14:24:11 +0800

    (test) make secret and expirationMs a local variable

--- src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
+++ src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
@@ -14,15 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class JwtUtilTest {
 
     private JwtUtil jwtUtil;
-    private String secret;
-    private long expirationMs;
 
     @BeforeEach
     void setUp() {
         jwtUtil = new JwtUtil();
         SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
-        secret = Base64.getEncoder().encodeToString(key.getEncoded());
-        expirationMs = 1000 * 60 * 60;
+        String secret = Base64.getEncoder().encodeToString(key.getEncoded());
+        long expirationMs = 1000 * 60 * 60;
 
         ReflectionTestUtils.setField(jwtUtil, "jwtSecret", secret);
         ReflectionTestUtils.setField(jwtUtil, "jwtExpirationMs", expirationMs);
