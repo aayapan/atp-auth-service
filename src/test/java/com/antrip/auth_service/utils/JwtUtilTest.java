0fef6c8404e0f31980d16b7a1efd0a72d781eeb1
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-29 14:18:20 +0800

    (test) setup test config for JwtUtilTest

--- src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
+++ src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
@@ -1,5 +1,26 @@
 package com.antrip.auth_service.utils;
 
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
 class JwtUtilTest {
 
+    private JwtUtil jwtUtil;
+    private String secret;
+    private long expirationMs;
+
+    @BeforeEach
+    void setUp() {
+        jwtUtil = new JwtUtil();
+        SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+        secret = Base64.getEncoder().encodeToString(key.getEncoded());
+        expirationMs = 1000 * 60 * 60;
+
+        ReflectionTestUtils.setField(jwtUtil, "jwtSecret", secret);
+        ReflectionTestUtils.setField(jwtUtil, "jwtExpirationMs", expirationMs);
+    }
 }
