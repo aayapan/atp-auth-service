cd2bd52b3d97771b70a2f92aa416a8579a482d98
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-06-29 14:20:37 +0800

    (test) add test for generating token and extracting username from token

--- src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
+++ src/test/java/com/antrip/auth_service/utils/JwtUtilTest.java
@@ -2,10 +2,14 @@ package com.antrip.auth_service.utils;
 
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilTest {
 
@@ -22,5 +26,16 @@ class JwtUtilTest {
 
         ReflectionTestUtils.setField(jwtUtil, "jwtSecret", secret);
         ReflectionTestUtils.setField(jwtUtil, "jwtExpirationMs", expirationMs);
+    }
+
+    @Test
+    @DisplayName("Should generate and extract username from token")
+    void testGenerateAndExtractToken() {
+        String username = "testuser";
+        String token = jwtUtil.generateToken(username);
+        assertNotNull(token);
+
+        String extractedUsername = jwtUtil.extractUsername(token);
+        assertEquals(username, extractedUsername);
     }
 }
