3b779d8c159b9c1579553ce21425f8b6bfbd6119
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-07-26 18:50:34 +0800

    (chore) add extra space

--- src/main/java/com/antrip/auth_service/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/antrip/auth_service/exceptions/UserAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.antrip.auth_service.exceptions;
 
-public class UserAlreadyExistsException extends RuntimeException{
+public class UserAlreadyExistsException extends RuntimeException {
 
     public UserAlreadyExistsException(String message) {
         super(message);
