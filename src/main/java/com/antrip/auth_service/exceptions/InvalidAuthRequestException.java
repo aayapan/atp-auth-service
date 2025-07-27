5638464a474a7edd875bb8e049907962c13a7529
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-07-27 20:47:42 +0800

    (chore) make it InvalidAuthRequestException to generalized the use of it

--- src/main/java/com/antrip/auth_service/exceptions/InvalidAuthRequestException.java
+++ src/main/java/com/antrip/auth_service/exceptions/InvalidAuthRequestException.java
@@ -3,11 +3,11 @@ package com.antrip.auth_service.exceptions;
 import lombok.Getter;
 
 @Getter
-public class InvalidRegisterRequestException extends RuntimeException {
+public class InvalidAuthRequestException extends RuntimeException {
 
     private final String field;
 
-    public InvalidRegisterRequestException(String message, String field) {
+    public InvalidAuthRequestException(String message, String field) {
         super(message);
         this.field = field;
     }
