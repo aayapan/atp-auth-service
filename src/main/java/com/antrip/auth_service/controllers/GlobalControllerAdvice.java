f6405901817b29d3a411cfff1969a926fd28eedd
Akio Yapan <akio.yapan@orangeandbronze.com>
2025-07-27 20:49:19 +0800

    (feat) handles BadCredentialsException and returns 401 unauthorized

--- src/main/java/com/antrip/auth_service/controllers/GlobalControllerAdvice.java
+++ src/main/java/com/antrip/auth_service/controllers/GlobalControllerAdvice.java
@@ -1,20 +1,27 @@
 package com.antrip.auth_service.controllers;
 
-import com.antrip.auth_service.exceptions.InvalidRegisterRequestException;
+import com.antrip.auth_service.exceptions.InvalidAuthRequestException;
 import com.antrip.auth_service.exceptions.UserAlreadyExistsException;
 import com.antrip.auth_service.models.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    @ExceptionHandler(InvalidRegisterRequestException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidRegisterRequest(InvalidRegisterRequestException ex) {
+    @ExceptionHandler(InvalidAuthRequestException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRegisterRequest(InvalidAuthRequestException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getField());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse("Invalid email or password", null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
